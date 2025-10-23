29e9473d857e5db6344888a0bd055023d34b0c38
meheraj786 <meherajhosen786@gmail.com>
2025-10-23 08:17:13 +0600

    setting up rtkq

---
+++ src/services/userApi.ts
@@ -0,0 +1,45 @@
+import { api } from "./api";
+
+export const userApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "/users",
+      providesTags: ["User"],
+    }),
+    getUserById: builder.query({
+      query: (id) => `/users/${id}`,
+      providesTags: ["User"],
+    }),
+    addUser: builder.mutation({
+      query: (newUser) => ({
+        url: "/users",
+        method: "POST",
+        body: newUser,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    updateUser: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["User"],
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useGetUserByIdQuery,
+  useAddUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = userApi;
---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "../services/api";
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
